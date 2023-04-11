8c436c6322a534b3ad72604a174ce05cf49a2e05
Giulianovf <giulianovf@hotmail.com>
2023-04-11 13:01:08 -0300

    add função e process no app

--- index.js
+++ index.js
@@ -6,12 +6,34 @@ const questions = [
 ]
 
 const ask = ( index = 0 ) => {
-    process.stdout.write(questions[index] + "\n\n\n")
+    process.stdout.write("\n" + questions[index] + " > ")
 }
 
 ask()
 
+const answer = []
 process.stdin.on("data", data => {
-    process.stdout.write( data.toString().trim() + '\n')
-    process.exit()
+    answer.push( data.toString().trim())
+    if (answer.length < questions.length) {
+        ask(answer.length)
+    } else {
+        process.exit()    
+    }
+})
+
+process.on('exit', () => {
+    console.log(`
+        Bacana Giuliano!
+
+        O que você aprendeu hoje foi:
+        ${answer[0]}
+
+        O que te aborreceu e você poderia melhorar hoje foi:
+        ${answer[1]}
+
+        O que te deixou feliz hoje:
+        ${answer[2]}
+
+        Você ajudou ${answer[3]} pessoas hoje!!
+    `)
 })
